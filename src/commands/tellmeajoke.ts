85ad1be20a60e499da634ada321835b04a64e68a
Abdelrahman Eweda <aeweda@kortobaa.com>
2019-12-05 12:57:19 +0200

    added new command tellmeajoke

---
+++ src/commands/tellmeajoke.ts
@@ -0,0 +1,26 @@
+import {Command, flags} from '@oclif/command'
+import { request } from 'http'
+
+export default class Tellmeajoke extends Command {
+
+  static description = 'command is used to retrieve a joke from programmableweb.com'
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+  }
+
+  async run() {
+    const {args, flags} = this.parse(Tellmeajoke)
+    const req = request(
+      {
+          host: 'programmableweb.com',
+          path: '/api/geek-jokes-rest-api',
+          method: 'GET',
+      },
+      response => {
+        console.log(response)
+      }
+    );
+    req.end();
+  }
+}
